b33b9b682b4d4d5bf53221ffea2d6d396b2d0f45
ClementLegouest <clement.legouest@gmail.com>
2019-05-22 00:01:48 +0200

    [evo] 22052019

--- cavino/src/app/pages/register/register.page.ts
+++ cavino/src/app/pages/register/register.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {RegisterService} from "../../services/register.service";
+import {AlertController, LoadingController} from "@ionic/angular";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  private firstname: string = "";
+  private lastname: string = "";
+  private email: string = "";
+  private password: string = "";
+  private password_bis: string = "";
+
+  private registerButtonStatus: boolean = false;
+  private registerResponse: Subscription;
+  private token: string = "";
+
+  constructor(
+      private registerService: RegisterService,
+      private loadCtrl: LoadingController,
+      private alrtCtrl: AlertController) { }
 
   ngOnInit() {
   }
 
+  register(firstname: string, lastname: string, email: string, password: string) {
+    if(this.password != this.password_bis) {
+      console.log("Mots de passe différents.")
+    } else {
+      this.registerButtonStatus = true;
+      this.registerResponse = this.registerService.register(firstname, lastname, email, password)
+          .subscribe((response) => {
+            console.log(response.token);
+            this.token = response.token;
+          });
+      this.registerButtonStatus = false;
+    }
+  }
 }
